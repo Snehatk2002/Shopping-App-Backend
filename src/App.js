18e37914598b37d1fc8ca3e50c827b01223c5022
Snehatk2002 <sammu5285@gmail.com>
2024-06-13 01:59:12 +0530

    updates done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Addshop from './components/Addshop';
 import SearchShop from './components/SearchShop';
+import ViewShop from './components/ViewShop';
 
 function App() {
   return (
     <div >
      <Addshop/>
      <SearchShop/>
+     <ViewShop/>
     </div>
   );
 }
